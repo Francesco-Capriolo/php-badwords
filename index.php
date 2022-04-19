10821ae42ecf15b869d759fa044aa95f66a66105
FrancescoCapriolo <francesco.capriolo92@gmail.com>
2022-04-19 16:59:26 +0200

    correzione esercizio php

--- index.php
+++ index.php
@@ -1,10 +1,26 @@
 <?php
     $paraghrap= "Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident, asperiores minima ipsam ad, obcaecati tenetur ab a adipisci, quod aperiam sint? Dolor vel quam, minus corrupti eum ipsam atque voluptatum.";
+    $name= $_GET['word'];
 ?>
-<pre>
-    <?php
-    var_dump($paraghrap);
-    var_dump(strlen($paraghrap));
-    var_dump(str_replace('asperiores','***', $paraghrap));
-    ?>
-</pre>
+
+<!DOCTYPE html>
+    <html lang = "it">
+
+    <head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title></title>
+    </head>
+    <body>
+        
+            <?php
+            echo $paraghrap;
+            echo '<br>';
+            echo strlen($paraghrap);
+            echo '<br>';
+            echo str_replace($name,'***', $paraghrap);
+            ?>
+    </body>
+
+</html>
